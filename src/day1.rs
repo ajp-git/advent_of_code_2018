bab2272bf2c75731bde473e73790eb61a5afed53
Alain Jean PARES <ajp@free.fr>
2024-03-08 16:20:48 +0100

    day 1 part 2 ok

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
@@ -16,3 +18,19 @@ fn input_generator(input: &str) -> Vec<i32> {
 fn solve_part1(input: &Vec<i32>) -> i32 {
     input.iter().sum()
 }
+
+#[aoc(day1, part2)]
+fn solve_part2(input: &Vec<i32>) -> i32 {
+    let mut freq:HashSet<i32>=HashSet::new();
+
+    let mut current_value = 0;
+    loop {
+        for val in input.iter() {
+            current_value+=val;
+            if ! freq.insert(current_value) {
+                return current_value;
+            }
+        }        
+    }
+}
+
