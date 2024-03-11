4c4869b3977b44200c9e09b29ae6836f38097c81
Alain Jean PARES <ajp@free.fr>
2024-03-11 09:11:08 +0100

    day 2 part 2 ok

--- src/day2.rs
+++ src/day2.rs
@@ -29,3 +29,28 @@ fn solve_part1(input: &Vec<String>) -> usize {
     }
     count2*count3
 }
+
+
+#[aoc(day2, part2)]
+fn solve_part2(input: &Vec<String>) -> String {
+
+    for x in input.iter(){
+        for y in input.iter() {
+            let mut diff=0;
+            let mut out:String=String::new();
+            for (c1, c2) in x.chars().zip(y.chars()) {
+                if c1!=c2 { 
+                    diff+=1
+                }
+                else {
+                    out.push(c1);
+                } 
+            }
+            if diff==1 {
+                println!("1 char diff between {} and {}", x, y);
+                return out;
+            }
+        }   
+    }
+    "".to_string()
+}
