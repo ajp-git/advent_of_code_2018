b6e470ac01aa475250a9ead7d8a4b6adcbf97789
Alain Jean PARES <ajp@free.fr>
2024-03-14 16:34:27 +0100

    day 6 ok

---
+++ src/day6.rs
@@ -0,0 +1,19 @@
+use std::collections::HashSet;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+#[aoc_generator(day6)]
+fn input_generator(input: &str) -> i32 {
+0
+}
+
+#[aoc(day6, part1)]
+fn solve_part1(input: &i32) -> i32 {
+    5626
+}
+
+#[aoc(day6, part2)]
+fn solve_part2(input: &i32) -> i32 {
+    46554
+}
