8f777a7b0eec9776216c7f0be5f5eb1049adb2f3
Alain Jean PARES <ajp@free.fr>
2024-04-04 10:40:48 +0200

    day 12 part 2 ok

--- src/day12.rs
+++ src/day12.rs
@@ -1,11 +1,61 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 #[derive(Debug,Clone)]
 struct Cave{
     pots:String,
     rules:HashSet<String>,
-    counter:u32,
+    generations:usize,
+
+}
+
+impl Cave {
+    
+    fn new(pots:String, rules:HashSet<String>) -> Self {
+        Cave { pots, rules, generations:0 }
+    }
+
+    fn run_generations(&mut self, generations:usize ) -> isize {
+
+        const CONVERGE:u32=10;
+
+        let mut n = String::from("...");
+        n.push_str(&self.pots);
+        n.push_str("...");
+
+        let mut last = 0;
+        let mut diffs: HashMap<isize, u32> = HashMap::new();
+
+        for gen in 1..=generations {
+            let mut out = String::from("...");
+            for i in 2..n.len() - 2 {
+                let slice = &n[i - 2..=i + 2];
+                if self.rules.get(slice).is_some(){
+                    out.push('#')
+                } else {
+                    out.push('.');
+                }
+                    }
+            out.push_str("...");
+            n = out;
+
+            // Our string grows by one '.' at both the beginning and end each generation
+            let score = n
+                .chars()
+                .enumerate()
+                .filter(|(_, c)| c == &'#')
+                .map(|(i, _)| i as isize - (3 + gen as isize))
+                .sum::<isize>();
+            let e = diffs.entry(score as isize - last as isize).or_insert(0);
+            if *e > CONVERGE {
+                return (generations - gen) as isize * (score - last) + score;
+            } else {
+                *e += 1;
+            }
+            last = score;
+        }
+        last    
+    }
 }
 
 #[aoc_generator(day12)]
@@ -51,44 +101,21 @@ fn input_generator(input: &str) -> Cave {
         }
     }
 
-    let mut cave=Cave{pots:first_line,rules:rules, counter:0};
+    let mut cave=Cave::new(first_line,rules);
     dbg!(&cave);
     cave
 }
 
 #[aoc(day12, part1)]
-fn solve_part1(input: &Cave) -> i32 {
+fn solve_part1(input: &Cave) -> isize {
 
-    let mut count:i32=0;
     let mut cave=input.clone();
-
-    for i in 0..20 {
-        cave.pots=format!("...{}...", cave.pots);
-        println!("{i}: {:?}", cave.pots);
-        run_generation(&mut cave);
-    }
-    let end =format!("{:?}", cave.pots.chars().skip(17).collect::<String>());
-
-    println!("Looking at {}", end);
-    for (i,c) in end.chars().enumerate() {
-        if c=='#' {
-            count+=i as i32-4;
-        }
-    }
-    count
+    cave.run_generations(20)
 }
 
-fn run_generation (cave: &mut Cave) {
+#[aoc(day12, part2)]
+fn solve_part2(input: &Cave) -> isize {
+    let mut cave=input.clone();
+    cave.run_generations(50_000_000_000)
 
-    let mut out:String=String::new();
-
-    for i in 0..=cave.pots.len()-5 {
-        let posts=cave.pots.chars().skip(i).take(5).collect::<String>();
-        if cave.rules.get(&posts).is_some(){
-            out.push('#')
-        } else {
-            out.push('.');
-        }
-    }
-    cave.pots=out;
 }
