eb1aa6748969027b7365c92d3df2306a9646067d
Alain Jean PARES <ajp@free.fr>
2024-04-08 07:18:23 +0200

    day14 part2

--- src/day14.rs
+++ src/day14.rs
@@ -65,6 +65,39 @@ fn print_recipes(index: u32, scores:Vec<u8>, elfe_1:usize, elfe_2:usize) {
 #[aoc(day14, part2)]
 fn solve_part2(input: &u32) -> String {
 
-    "".to_string()
+    let input=*input;
+
+    let mut scores:Vec<u8>=Vec::new();
+    scores.push(3);
+    scores.push(7);
+    
+    let mut elfe_1:usize=0;
+    let mut elfe_2:usize=1;
+    print_recipes(0,scores.clone(), elfe_1, elfe_2);
+   
+    let mut count=0;
+    loop {
+        let total=scores[elfe_1 as usize]+scores[elfe_2 as usize];
+
+        let digit_1=total/10;
+        if digit_1 >0 {
+            scores.push(digit_1);
+        }
+        let digit_2=total%10;
+        scores.push(digit_2);
+
+        elfe_1=(elfe_1+1+scores[elfe_1] as usize)%scores.len();
+        elfe_2=(elfe_2+1+scores[elfe_2] as usize)%scores.len();
+
+//        print_recipes(count, scores.clone(), elfe_1, elfe_2);
+        count+=1;
+        if count==(input+1)*2 {
+            let mut s:String=String::new();
+            for i in input..input+6 {
+                s.push_str(format!("{}",scores[i as usize]).as_str());
+            }
+            return s;
+        }
+    }
 }
 
