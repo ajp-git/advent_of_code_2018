24ca4bc7e8a8d3847b4e3409b3542fdb4c5f8a38
Alain Jean PARES <ajp@free.fr>
2024-04-11 10:49:12 +0200

    switching to TDD

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
+use std::{cmp::max, cmp::min, collections::HashMap};
 
 use aoc_runner_derive::{aoc, aoc_generator};
-
 
 enum Grid {
     Wall,
@@ -11,6 +10,8 @@ enum Grid {
 }
 struct Cave {
     grid:HashMap<(usize,usize),Grid>,
+    max_x:usize,
+    max_y:usize,
 }
 
 impl Cave {
@@ -29,24 +30,110 @@ impl Cave {
                 };
             }
         }
-        Cave { grid:grid }
+
+        let max_x=*grid.keys().map(|(x,_)|x).max().unwrap();
+        let max_y=*grid.keys().map(|(_,y)|y).max().unwrap();
+       
+        Cave { grid, max_x, max_y }
     }
 
     fn print(&self) {
-        let max_x=self.grid.keys().map(|(x,_)|x).max().unwrap();
-        let max_y=self.grid.keys().map(|(_,y)|y).max().unwrap();
-        
+
+        for y in 0..=self.max_y {
+            for x in 0..=self.max_x{
+                print!("{}",
+                match self.grid.get(&(x,y)).unwrap() {
+                    Grid::Wall => '#',
+                    Grid::Elf(v) => 'E',
+                    Grid::Goblin(v) => 'G',
+                    Grid::Open => '.',
+                });
+            }
+            println!();
+        }
     }
+
+    fn turn(self: &mut Self){
+
+        // starting from top left
+        // for each G ot E
+        // find nearest target
+        // step to it
+
+        for y in 0..=self.max_y{
+            for x in 0..=self.max_x{
+                let current=self.grid.get(&(x,y)).unwrap();
+                match current {
+                    Grid::Elf(_) => {},
+                    Grid::Goblin(_) => {},
+                    _ => {},
+                }
+            }
+        }
+    }
+
+    fn get_nearest_target(&self, x:usize, y:usize) -> Option<(usize,usize)> {
+
+        let mut x_dist=usize::MAX;
+        let mut y_dist=usize::MAX;
+
+        let current=self.grid.get(&(x,y)).unwrap().clone();
+        let target:Grid;
+
+        match current {
+            Grid::Elf(_) => target=Grid::Goblin(0),
+            Grid::Goblin(_) => target=Grid::Elf(0),
+            _ => panic!("Impossible current"),
+            
+        }
+
+        let circle_size=max(
+            x.max(self.max_x-x),
+            y.max(self.max_y-y));
+
+
+        // turn around 
+        // (-1,-1) (0,-1)  (1,-1)
+        // (-1, 0) (x,y)   (1,0)
+        // (-1, 1) (0,1)   (1,1)
+
+        for i in 1..circle_size{
+            for nx in 0.max(x-i)..self.max_x.min(x+i) {
+                for ny in 0.max(x-i)..self.max_x.min(x+i) {
+                    match (current,self.grid.get(&(nx,ny)).unwrap()) {
+                        (Grid::Elf(_), Grid::Goblin(_)) => if self.is_accessible_target(x, y, nx, ny) { return Some((nx, ny)); }, 
+                        (Grid::Goblin(_), Grid::Elf(_)) => if self.is_accessible_target(x, y, nx, ny) { return Some((nx, ny)); }, 
+                        _ =>{},
+                    }
+                }   
+            }
+        }
+        None
+    }
+
+    fn is_accessible_target(&self, src_x:usize, src_y:usize, target_x:usize, target_y:usize) -> bool {
+        // is there an open path ?
+        true
+    }
+
 }
 
 #[aoc_generator(day15)]
 fn input_generator(input: &str) -> Cave {
+    let input="#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######";
 
     Cave::new(input)
 }
 
 #[aoc(day15, part1)]
 fn solve_part1(input: &Cave) -> u32 {
+    input.print();
     0
 }
 
