4149db19dcc1e999fe39ff40de9b5cf1e8949e90
AJ PARES <ajp@p997.net>
2024-08-13 07:11:57 +0200

    Day 15 restart

--- src/day15.rs
+++ src/day15.rs
@@ -1,119 +1,97 @@
-use std::{cmp::max, cmp::min, collections::HashMap};
+use std::{cmp::{max, min, Ordering}, collections::HashMap};
 
 use aoc_runner_derive::{aoc, aoc_generator};
-
-enum Grid {
+#[derive(Debug,PartialEq)]
+enum Tile {
     Wall,
     Open,
-    Goblin(u32),
-    Elf(u32),
+    Unit(UnitType),
+}
+
+#[derive(Debug,PartialEq, Clone, Copy)]
+enum UnitType {
+    Goblin,
+    Elf,
+}
+#[derive(Debug,PartialEq)]
+struct Unit{
+    unittype:UnitType,
+    health:u32,
+    x:usize,
+    y:usize,
+    attack:u32,
 }
 struct Cave {
-    grid:HashMap<(usize,usize),Grid>,
-    max_x:usize,
-    max_y:usize,
+    grid:Vec<Vec<Tile>>,
+    units:Vec<Unit>,
 }
 
 impl Cave {
     fn new (input: &str) -> Self {
 
-        let mut grid:HashMap<(usize,usize), Grid>=HashMap::new();
-    
-        for (y, line) in input.lines().enumerate(){
-            for (x, c) in line.chars().enumerate() {
-                match c {
-                    '#' => grid.insert((x,y), Grid::Wall),
-                    '.' => grid.insert((x,y), Grid::Open),
-                    'G' => grid.insert((x,y), Grid::Goblin(200)),
-                    'E' => grid.insert((x,y), Grid::Elf(200)),
-                    _ => panic!("unknown char {} in grid", c),
-                };
+        let mut grid=Vec::new();
+        let mut units:Vec<Unit>=Vec::new();
+
+        for line in input.trim().lines(){
+            let mut row=Vec::new();
+            for c in line.chars() {
+                let tile=
+                    match c {
+                        '#' => Tile::Wall,
+                        '.' => Tile::Open,
+                        'G' => Tile::Unit(UnitType::Goblin),
+                        'E' => Tile::Unit(UnitType::Elf),
+                        _ => panic!("unknown char {} in grid", c),
+                    };
+                row.push(tile);
             }
+            grid.push(row);
         }
 
-        let max_x=*grid.keys().map(|(x,_)|x).max().unwrap();
-        let max_y=*grid.keys().map(|(_,y)|y).max().unwrap();
-       
-        Cave { grid, max_x, max_y }
+        for y in 0..grid.len(){
+            for x in 0..grid[0].len() {
+                match &grid[y][x] {
+                    Tile::Unit(v) => {
+                        units.push(Unit { 
+                            unittype: *v,
+                            health: 200,
+                            x:x, y:y,
+                            attack: 3 } );
+                    },
+                    _ => {},
+                }
+            }
+        }
+        Cave { grid, units }
     }
 
     fn print(&self) {
 
-        for y in 0..=self.max_y {
-            for x in 0..=self.max_x{
+        for y in 0..self.grid.len() {
+            for x in 0..self.grid[0].len(){
                 print!("{}",
-                match self.grid.get(&(x,y)).unwrap() {
-                    Grid::Wall => '#',
-                    Grid::Elf(v) => 'E',
-                    Grid::Goblin(v) => 'G',
-                    Grid::Open => '.',
+                match self.grid[y][x]{
+                    Tile::Wall => '#',
+                    Tile::Unit(UnitType::Elf) => 'E',
+                    Tile::Unit(UnitType::Goblin) => 'G',
+                    Tile::Open => '.',
                 });
             }
             println!();
         }
     }
 
-    fn turn(self: &mut Self){
+    fn sort_units(self: &mut Self){
 
-        // starting from top left
-        // for each G ot E
-        // find nearest target
-        // step to it
-
-        for y in 0..=self.max_y{
-            for x in 0..=self.max_x{
-                let current=self.grid.get(&(x,y)).unwrap();
-                match current {
-                    Grid::Elf(_) => {},
-                    Grid::Goblin(_) => {},
-                    _ => {},
-                }
+        self.units.sort_by(|a,b| 
+            if a.y < b.y {
+                Ordering::Less
+            } else if a.y==b.y && a.x < b.x {
+                Ordering::Less
+            } else {
+                Ordering::Greater
             }
-        }
-    }
-
-    fn get_nearest_target(&self, x:usize, y:usize) -> Option<(usize,usize)> {
-
-        let mut x_dist=usize::MAX;
-        let mut y_dist=usize::MAX;
-
-        let current=self.grid.get(&(x,y)).unwrap().clone();
-        let target:Grid;
-
-        match current {
-            Grid::Elf(_) => target=Grid::Goblin(0),
-            Grid::Goblin(_) => target=Grid::Elf(0),
-            _ => panic!("Impossible current"),
-            
-        }
-
-        let circle_size=max(
-            x.max(self.max_x-x),
-            y.max(self.max_y-y));
-
-
-        // turn around 
-        // (-1,-1) (0,-1)  (1,-1)
-        // (-1, 0) (x,y)   (1,0)
-        // (-1, 1) (0,1)   (1,1)
-
-        for i in 1..circle_size{
-            for nx in 0.max(x-i)..self.max_x.min(x+i) {
-                for ny in 0.max(x-i)..self.max_x.min(x+i) {
-                    match (current,self.grid.get(&(nx,ny)).unwrap()) {
-                        (Grid::Elf(_), Grid::Goblin(_)) => if self.is_accessible_target(x, y, nx, ny) { return Some((nx, ny)); }, 
-                        (Grid::Goblin(_), Grid::Elf(_)) => if self.is_accessible_target(x, y, nx, ny) { return Some((nx, ny)); }, 
-                        _ =>{},
-                    }
-                }   
-            }
-        }
-        None
-    }
-
-    fn is_accessible_target(&self, src_x:usize, src_y:usize, target_x:usize, target_y:usize) -> bool {
-        // is there an open path ?
-        true
+        );
     }
 
 }
@@ -143,3 +121,65 @@ fn solve_part2(input: &Cave) -> u32 {
     0
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_cave_tiles() -> Cave {
+        let input = "\
+        #######\n\
+        #.G.E.#\n\
+        #E.G.E#\n\
+        #.G.E.#\n\
+        #######";
+
+        Cave::new(input)
+    }
+
+    #[test]
+    fn test_cave_creation() {
+
+        let cave=create_cave_tiles();
+
+        // Check specific tiles
+        assert_eq!(cave.grid[0][0], Tile::Wall); // Top-left corner should be a wall
+        assert_eq!(cave.grid[1][2], Tile::Unit(UnitType::Goblin)); // There should be a Goblin at (2, 1)
+        assert_eq!(cave.grid[1][4], Tile::Unit(UnitType::Elf)); // There should be an Elf at (4, 1)
+        assert_eq!(cave.grid[1][1], Tile::Open); // There should be an open space at (1, 1)
+    }
+    #[test]
+    fn test_unit_count() {
+
+        let cave=create_cave_tiles();
+
+        // Check specific tiles
+        assert_eq!(cave.
+            grid
+            .iter()
+            .flatten()
+            .filter(
+                |&t|
+                *t==Tile::Unit(UnitType::Elf) || *t==Tile::Unit(UnitType::Goblin))
+            .count(), 7); // Top-left corner should be a wall
+    }
+
+    #[test]
+    fn test_sort_units() {
+        let mut cave=create_cave_tiles();
+        cave.sort_units();
+
+        let mut units=cave.units.iter();
+        units.next();
+        units.next();
+        let u=units.next().unwrap();
+        assert_eq!(u.x, 1)
+    }
+
+    #[test]
+    fn test_find_next_move() {
+        let mut cave=create_cave_tiles();
+        cave.sort_units();
+
+        
+    }
+}
