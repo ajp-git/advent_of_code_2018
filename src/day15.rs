961b14d6f554d459cb20897e8b6e0de66d06afaa
AJ PARES <ajp@p997.net>
2024-08-21 13:39:58 +0200

    day 15 and 16

--- src/day15.rs
+++ src/day15.rs
@@ -1,104 +1,247 @@
-use std::{cmp::{max, min, Ordering}, collections::HashMap};
+use std::{
+    cmp::{max, min, Ordering},
+    collections::{HashMap, HashSet, VecDeque},
+};
 
 use aoc_runner_derive::{aoc, aoc_generator};
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Tile {
     Wall,
     Open,
     Unit(UnitType),
 }
+type Position = (usize, usize);
 
-#[derive(Debug,PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy, Eq, PartialOrd, Ord)]
 enum UnitType {
     Goblin,
     Elf,
 }
-#[derive(Debug,PartialEq)]
-struct Unit{
-    unittype:UnitType,
-    health:u32,
-    x:usize,
-    y:usize,
-    attack:u32,
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+struct Unit {
+    id: u32,
+    unittype: UnitType,
+    health: u32,
+    x: usize,
+    y: usize,
+    attack: u32,
 }
 struct Cave {
-    grid:Vec<Vec<Tile>>,
-    units:Vec<Unit>,
+    grid: Vec<Vec<Tile>>,
+    units: Vec<Unit>,
+    rounds: usize,
 }
 
 impl Cave {
-    fn new (input: &str) -> Self {
+    fn new(input: &str) -> Self {
+        let mut grid = Vec::new();
+        let mut units: Vec<Unit> = Vec::new();
 
-        let mut grid=Vec::new();
-        let mut units:Vec<Unit>=Vec::new();
-
-        for line in input.trim().lines(){
-            let mut row=Vec::new();
+        for line in input.trim().lines() {
+            let mut row = Vec::new();
             for c in line.chars() {
-                let tile=
-                    match c {
-                        '#' => Tile::Wall,
-                        '.' => Tile::Open,
-                        'G' => Tile::Unit(UnitType::Goblin),
-                        'E' => Tile::Unit(UnitType::Elf),
-                        _ => panic!("unknown char {} in grid", c),
-                    };
+                let tile = match c {
+                    '#' => Tile::Wall,
+                    '.' => Tile::Open,
+                    'G' => Tile::Unit(UnitType::Goblin),
+                    'E' => Tile::Unit(UnitType::Elf),
+                    _ => panic!("unknown char {} in grid", c),
+                };
                 row.push(tile);
             }
             grid.push(row);
         }
+        let mut id = 0;
 
-        for y in 0..grid.len(){
+        for y in 0..grid.len() {
             for x in 0..grid[0].len() {
                 match &grid[y][x] {
                     Tile::Unit(v) => {
-                        units.push(Unit { 
+                        units.push(Unit {
+                            id,
                             unittype: *v,
                             health: 200,
-                            x:x, y:y,
-                            attack: 3 } );
-                    },
-                    _ => {},
+                            x: x,
+                            y: y,
+                            attack: 3,
+                        });
+                        id += 1;
+                    }
+                    _ => {}
                 }
             }
         }
-        Cave { grid, units }
+        Cave {
+            grid,
+            units,
+            rounds: 0,
+        }
     }
 
     fn print(&self) {
-
         for y in 0..self.grid.len() {
-            for x in 0..self.grid[0].len(){
-                print!("{}",
-                match self.grid[y][x]{
-                    Tile::Wall => '#',
-                    Tile::Unit(UnitType::Elf) => 'E',
-                    Tile::Unit(UnitType::Goblin) => 'G',
-                    Tile::Open => '.',
-                });
+            for x in 0..self.grid[0].len() {
+                print!(
+                    "{}",
+                    match self.grid[y][x] {
+                        Tile::Wall => '#',
+                        Tile::Unit(UnitType::Elf) => 'E',
+                        Tile::Unit(UnitType::Goblin) => 'G',
+                        Tile::Open => '.',
+                    }
+                );
             }
             println!();
         }
     }
 
-    fn sort_units(self: &mut Self){
-
-        self.units.sort_by(|a,b| 
+    fn sort_units(self: &mut Self) {
+        self.units.sort_by(|a, b| {
             if a.y < b.y {
                 Ordering::Less
-            } else if a.y==b.y && a.x < b.x {
+            } else if a.y == b.y && a.x < b.x {
                 Ordering::Less
             } else {
                 Ordering::Greater
             }
-        );
+        });
     }
 
+    fn find_target_for_unit(&self, unit: &Unit) -> Option<(u32, usize)> {
+        // for all targets, get shortest path
+        let possible_targets: Vec<Unit> = self
+            .units
+            .iter()
+            .filter(|u| u.unittype != unit.unittype)
+            .copied()
+            .collect();
+
+        let target = possible_targets
+            .iter()
+            .map(|u| (u.id, self.get_target_distance(unit, u)))
+            .min_by_key(|(_, d)| d.clone());
+
+        if target.is_some() {
+            return target.unwrap().0;
+        }
+        None
+    }
+
+    fn get_target_distance(&self, src_unit: &Unit, dst_unit: &Unit) -> Option<usize> {
+        if let Some(path) = get_path(
+            &self.grid,
+            (src_unit.x, src_unit.y),
+            (dst_unit.x, dst_unit.y),
+        ) {
+            return Some(path.0);
+        } else {
+            None
+        }
+    }
+
+    fn get_target_1st_move(&self, src_unit: &Unit, dst_unit: &Unit) -> Option<Position> {
+        if let Some(path) = get_path(
+            &self.grid,
+            (src_unit.x, src_unit.y),
+            (dst_unit.x, dst_unit.y),
+        ) {
+            let steps=path.1;
+            while prev=steps.
+            return Some(path.0,path.1.get(k));
+        } else {
+            None
+        }
+    }
+
+    fn run(&mut self) -> u32 {
+        while self
+            .units
+            .iter()
+            .filter(|&&u| u.unittype == UnitType::Elf && u.health > 0)
+            .count()
+            > 0
+        {
+            self.round();
+        }
+        self.units
+            .iter()
+            .filter(|u| u.health > 0)
+            .map(|u| u.health)
+            .sum()
+    }
+
+    fn round(&mut self) {
+        self.units.sort();
+
+        for unit in self.units.iter() {
+            if let target = self.find_target_for_unit(unit) {
+                // Handle targer
+                todo!();
+            } else {
+                // No target means blocked or alone
+                todo!();
+            }
+        }
+    }
+}
+
+fn get_path(
+    grid: &Vec<Vec<Tile>>,
+    src: Position,
+    destination: Position,
+) -> Option<(usize, HashMap<Position, Position>)> {
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+    let mut prev: HashMap<Position, Position> = HashMap::new();
+    queue.push_back((src, 0));
+
+    while queue.len() > 0 {
+        if let Some(current) = queue.pop_front() {
+            seen.insert(current.0);
+            if current.0 == destination {
+                return Some((current.1, prev));
+            }
+            let next_moves = get_available_neighbors(grid, current.0);
+            next_moves.iter().for_each(|n| {
+                if !seen.contains(n) {
+                    queue.push_back((*n, current.1 + 1));
+                    prev.insert(*n, current.0);
+                }
+            });
+        }
+    }
+    None
+}
+fn get_available_neighbors(grid: &Vec<Vec<Tile>>, pos: Position) -> Vec<Position> {
+    let mut positions: Vec<Position> = Vec::new();
+
+    let (x, y) = pos;
+    if x > 0 {
+        if grid[y][x - 1] == Tile::Open {
+            positions.push((x - 1, y));
+        }
+    }
+    if y > 0 {
+        if grid[y - 1][x] == Tile::Open {
+            positions.push((x, y - 1));
+        }
+    }
+    if x < grid.len() - 1 {
+        if grid[y][x + 1] == Tile::Open {
+            positions.push((x + 1, y));
+        }
+    }
+    if y < grid[0].len() - 1 {
+        if grid[y + 1][x] == Tile::Open {
+            positions.push((x, y + 1));
+        }
+    }
+    positions
 }
 
 #[aoc_generator(day15)]
 fn input_generator(input: &str) -> Cave {
-    let input="#######
+    let input = "#######
 #.G...#
 #...EG#
 #.#.#G#
@@ -117,12 +260,13 @@ fn solve_part1(input: &Cave) -> u32 {
 
 #[aoc(day15, part2)]
 fn solve_part2(input: &Cave) -> u32 {
-    
     0
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use super::*;
 
     fn create_cave_tiles() -> Cave {
@@ -138,8 +282,7 @@ mod tests {
 
     #[test]
     fn test_cave_creation() {
-
-        let cave=create_cave_tiles();
+        let cave = create_cave_tiles();
 
         // Check specific tiles
         assert_eq!(cave.grid[0][0], Tile::Wall); // Top-left corner should be a wall
@@ -149,37 +292,42 @@ mod tests {
     }
     #[test]
     fn test_unit_count() {
-
-        let cave=create_cave_tiles();
+        let cave = create_cave_tiles();
 
         // Check specific tiles
-        assert_eq!(cave.
-            grid
-            .iter()
-            .flatten()
-            .filter(
-                |&t|
-                *t==Tile::Unit(UnitType::Elf) || *t==Tile::Unit(UnitType::Goblin))
-            .count(), 7); // Top-left corner should be a wall
+        assert_eq!(
+            cave.grid
+                .iter()
+                .flatten()
+                .filter(|&t| *t == Tile::Unit(UnitType::Elf) || *t == Tile::Unit(UnitType::Goblin))
+                .count(),
+            7
+        ); // Top-left corner should be a wall
     }
 
     #[test]
     fn test_sort_units() {
-        let mut cave=create_cave_tiles();
+        let mut cave = create_cave_tiles();
         cave.sort_units();
 
-        let mut units=cave.units.iter();
+        let mut units = cave.units.iter();
         units.next();
         units.next();
-        let u=units.next().unwrap();
+        let u = units.next().unwrap();
         assert_eq!(u.x, 1)
     }
 
     #[test]
     fn test_find_next_move() {
-        let mut cave=create_cave_tiles();
+        let mut cave = create_cave_tiles();
         cave.sort_units();
+    }
 
-        
+    #[test]
+    fn test_available_positions() {
+        let cave = create_cave_tiles();
+        assert_eq!(get_available_neighbors(&cave.grid, (0, 0)).len(), 0);
+        assert_eq!(get_available_neighbors(&cave.grid, (1, 1)).len(), 0);
+        assert_eq!(get_available_neighbors(&cave.grid, (2, 1)).len(), 3);
     }
 }
---
+++ src/day16.rs
@@ -0,0 +1,500 @@
+use std::collections::HashMap;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+use regex::Regex;
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+struct Operation {
+    name: String,
+    id: u8,
+}
+
+#[derive(Debug)]
+struct TrainData {
+    /*
+       Before: [0, 2, 2, 2]
+       11 3 3 3
+       After:  [0, 2, 2, 0]
+    */
+    before: Vec<u8>,
+    values: Vec<u8>,
+    after: Vec<u8>,
+}
+#[derive(Debug)]
+struct RealData {
+    values: Vec<u8>,
+}
+
+#[aoc_generator(day16)]
+fn input_generator(input: &str) -> (Vec<TrainData>, Vec<RealData>) {
+    let mut train: Vec<TrainData> = Vec::new();
+    let mut data: Vec<RealData> = Vec::new();
+
+    let mut lines = input.lines();
+    let re_values = Regex::new(r"(\d+) (\d+) (\d+) (\d+)").unwrap();
+
+    while let Some(line) = lines.next() {
+        if line.starts_with(&"Before") {
+            let re_before = Regex::new(r"Before: \[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
+            let values_before = re_before.captures(&line);
+            if values_before.is_none() {
+                println!("{} not regexed", line);
+            }
+            let values_before = values_before.unwrap();
+            let line = lines.next().unwrap();
+            let values = re_values.captures(&line).unwrap();
+
+            let line = lines.next().unwrap();
+            let re_after = Regex::new(r"After:  \[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
+            let values_after = re_after.captures(&line).unwrap();
+            lines.next();
+
+            train.push(TrainData {
+                before: (1..=4)
+                    .map(|i| values_before[i].parse::<u8>().unwrap())
+                    .collect(),
+                values: (1..=4).map(|i| values[i].parse::<u8>().unwrap()).collect(),
+                after: (1..=4)
+                    .map(|i| values_after[i].parse::<u8>().unwrap())
+                    .collect(),
+            });
+        } else {
+            if line.to_string().len() > 0 {
+                let values = re_values.captures(&line).unwrap();
+                data.push(RealData {
+                    values: (1..=4).map(|i| values[i].parse::<u8>().unwrap()).collect(),
+                });
+            }
+        }
+    }
+    (train, data)
+}
+
+fn execute_op(regs: &Vec<u32>, operations: &Vec<Operation>, command: &Vec<u8>) -> Vec<u32> {
+    let mut new_regs = regs.clone();
+    let command: Vec<usize> = command.iter().map(|v| *v as usize).collect();
+    let operation = operations
+        .iter()
+        .find(|operation| operation.id == command[0] as u8)
+        .expect("msg: operation not found");
+    match operation.name.as_str() {
+        "addr" => {
+            new_regs[command[3]] = new_regs[command[1]] + new_regs[command[2]];
+        }
+        "addi" => {
+            new_regs[command[3]] = new_regs[command[1]] + command[2] as u32;
+        }
+        "mulr" => {
+            new_regs[command[3]] = new_regs[command[1]] * new_regs[command[2]];
+        }
+        "muli" => {
+            new_regs[command[3]] = new_regs[command[1]] * command[2] as u32;
+        }
+        "banr" => {
+            new_regs[command[3]] = new_regs[command[1]] & new_regs[command[2]];
+        }
+        "bani" => {
+            new_regs[command[3]] = new_regs[command[1]] & command[2] as u32;
+        }
+        "borr" => {
+            new_regs[command[3]] = new_regs[command[1]] | new_regs[command[2]];
+        }
+        "bori" => {
+            new_regs[command[3]] = new_regs[command[1]] | command[2] as u32;
+        }
+        "setr" => {
+            new_regs[command[3]] = new_regs[command[1]];
+        }
+        "seti" => {
+            new_regs[command[3]] = command[1] as u32;
+        }
+        "gtir" => {
+            new_regs[command[3]] = if command[1] as u32 > new_regs[command[2]] {
+                1
+            } else {
+                0
+            };
+        }
+        "gtri" => {
+            new_regs[command[3]] = if new_regs[command[1]] > command[2] as u32 {
+                1
+            } else {
+                0
+            };
+        }
+        "gtrr" => {
+            new_regs[command[3]] = if new_regs[command[1]] > new_regs[command[2]] {
+                1
+            } else {
+                0
+            };
+        }
+        "eqir" => {
+            new_regs[command[3]] = if command[1] as u32 == new_regs[command[2]] {
+                1
+            } else {
+                0
+            };
+        }
+        "eqri" => {
+            new_regs[command[3]] = if new_regs[command[1]] == command[2] as u32 {
+                1
+            } else {
+                0
+            };
+        }
+        "eqrr" => {
+            new_regs[command[3]] = if new_regs[command[1]] == new_regs[command[2]] {
+                1
+            } else {
+                0
+            };
+        }
+
+        _ => panic!("Unknown command {:?}", operation.name),
+    }
+    new_regs
+}
+
+fn simulate(train: &Vec<TrainData>) -> (usize, Vec<Operation>) {
+    let mut operations: Vec<Operation> = vec![
+        ("addr", 0u8),
+        ("addi", 1),
+        ("mulr", 2),
+        ("muli", 3),
+        ("banr", 4),
+        ("bani", 5),
+        ("borr", 6),
+        ("bori", 7),
+        ("setr", 8),
+        ("seti", 9),
+        ("gtir", 10),
+        ("gtri", 11),
+        ("gtrr", 12),
+        ("eqir", 13),
+        ("eqri", 14),
+        ("eqrr", 15),
+    ]
+    .iter()
+    .map(|(name, id)| Operation {
+        name: name.to_string(),
+        id: *id,
+    })
+    .collect();
+
+    let mut proba: HashMap<(usize, Vec<u8>), u32> = HashMap::new();
+    let mut regs: Vec<u32> = vec![0; 4];
+    #[derive(Debug, Clone, PartialEq)]
+    struct Possibilities {
+        id: u8,
+        ids: Vec<u8>,
+    }
+    let mut posssibilities: Vec<Possibilities> = (0..16)
+        .map(|i| Possibilities {
+            id: i,
+            ids: (0..16).collect(),
+        })
+        .collect();
+
+    for (index, train_line) in train.iter().enumerate() {
+        // assign registers
+        train_line
+            .before
+            .iter()
+            .enumerate()
+            .for_each(|(i, &v)| regs[i] = v as u32);
+
+        for op in 0..16 {
+            let mut train_line_updated = TrainData {
+                before: train_line.before.clone(),
+                values: vec![
+                    op,
+                    train_line.values[1],
+                    train_line.values[2],
+                    train_line.values[3],
+                ],
+                after: train_line.after.clone(),
+            };
+            train_line_updated.values[0] = op;
+            let new_regs = execute_op(&regs, &operations, &train_line_updated.values);
+            if train_line
+                .after
+                .iter()
+                .enumerate()
+                .filter(|(i, &v)| new_regs[*i] == v as u32)
+                .count()
+                == 4
+            {
+                // Good match
+                /*                println!(
+                    "{} {} is a good match for line {:?}",
+                    op, operations[op as usize].name, train_line
+                ); */
+                let key = (index, train_line.values.clone());
+                *proba.entry(key).or_insert(0) += 1;
+            } else {
+                /*if train_line.values[0] == 0 {
+                    println!("Removing {} from {}", op, train_line.values[0]);
+                }*/
+                posssibilities
+                    .get_mut(train_line.values[0] as usize)
+                    .unwrap()
+                    .ids
+                    .retain(|&f| f != op);
+            }
+        }
+    }
+
+    loop {
+        let mut new_posssibilities = posssibilities.clone();
+        let mut changed = false;
+        for (i, pos) in posssibilities.iter().enumerate() {
+            if pos.ids.len() == 1 {
+                let remove = pos.ids[0];
+                //println!("Removing {}", remove);
+                for (j, other_pos) in new_posssibilities.iter_mut().enumerate() {
+                    if i != j {
+                        let original_len = other_pos.ids.len();
+                        other_pos.ids.retain(|&f| f != remove);
+                        if original_len != other_pos.ids.len() {
+                            changed = true;
+                        }
+                    }
+                }
+            }
+        }
+        posssibilities = new_posssibilities;
+        if !changed {
+            break;
+        }
+    }
+    /*for i in 0..posssibilities.len() {
+        println!("Possibilities of {i} : {:?}", posssibilities[i].ids);
+    }*/
+    operations
+        .iter_mut()
+        .zip(posssibilities)
+        .for_each(|(o, p)| o.id = p.ids[0]);
+    (proba.iter().filter(|&l| l.1 >= &3).count(), operations)
+}
+
+#[aoc(day16, part1)]
+fn solve_part1((train, values): &(Vec<TrainData>, Vec<RealData>)) -> usize {
+    simulate(train).0
+    //println!("Training : {:?}", train);
+    //println!("Data : {:?}", values);
+}
+#[aoc(day16, part2)]
+fn solve_part2((train, values): &(Vec<TrainData>, Vec<RealData>)) -> u32 {
+    let operations = simulate(train).1;
+
+    let mut regs = vec![0u32; 4];
+    values.iter().for_each(|v| {
+        regs = execute_op(&regs, &operations, &v.values);
+        if let Some(op) = operations.iter().find(|x| x.id == v.values[0]) {
+            println!("Op : {:?}\tline : {:?}\tregs {:?}", op.name, v.values, regs);
+        }
+    });
+    println!("Operations : {:?}", operations);
+    //println!("Training : {:?}", train);
+    //println!("Data : {:?}", values);
+    regs[0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_addr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "addr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 5];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_addi() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "addi".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 5, 4];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_mulr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "mulr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 6];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_muli() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "muli".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 6, 4];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_banr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "banr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 2];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_bani() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "bani".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 0];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_borr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "borr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 3];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_bori() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "bori".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 3];
+        let expected = vec![1, 2, 3, 3];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_setr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "setr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 0, 3];
+        let expected = vec![1, 2, 3, 2];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_seti() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "seti".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 0, 3];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_gtir() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "gtir".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 3, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_gtri() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "gtri".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_gtrr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "gtrr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_eqir() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "eqir".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 2, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_eqri() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "eqri".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+
+    #[test]
+    fn test_eqrr() {
+        let regs = vec![1, 2, 3, 4];
+        let operations = vec![Operation {
+            name: "eqrr".to_string(),
+            id: 1,
+        }];
+        let command = vec![1, 1, 2, 2];
+        let expected = vec![1, 2, 3, 1];
+        assert_eq!(execute_op(&regs, &operations, &command), expected);
+    }
+}
